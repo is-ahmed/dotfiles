375ab36c25b7fef3e05f2dec372f17e59c450e2f
Ismail Ahmed <isahmed0149@gmail.com>
2023-04-12 08:15:07 -0400

    Tab through nvim-cmp suggestions

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -21,6 +21,13 @@ local cmp = require'cmp'
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	  ['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+	  end)
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
