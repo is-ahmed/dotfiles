70ea2b34074c0caae1053366b681b81471e2e862
Ismail Ahmed <isahmed0149@gmail.com>
2023-04-14 20:43:19 -0400

    Add bindings for moving between git hunks

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,6 +25,9 @@ vim.api.nvim_set_keymap('n', '<leader>sh', ':Gitsigns stage_hunk<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>uh', ':Gitsigns undo_stage_hunk<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>ph', ':Gitsigns preview_hunk<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>rh', ':Gitsigns reset_hunk<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>n', ':Gitsigns next_hunk<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>N', ':Gitsigns prev_hunk<cr>', {})
+
 
 vim.cmd('set number relativenumber')
 vim.keymap.set("x", "p", function() return 'pgv"' .. vim.v.register .. "y" end, { remap = false, expr = true })
