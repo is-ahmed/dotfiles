65235d5aa9d1699f34f620f4be445175d0cc319d
Ismail Ahmed <isahmed@pop-os.localdomain>
2023-11-19 13:59:33 -0500

    Stop continuing comments on new line

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,5 +57,12 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern="*",
+	callback = function ()
+		vim.opt.formatoptions:remove({"c", "r", "o"})
+	end
+})
+
 require('packer-plugins')
 
